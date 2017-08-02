ad6e4a70db925ac44c6ce371ca23f19d24040226
Joshua Rose <joshua.rose@outlook.com>
2017-08-02 18:37:11 +0100

    Document AnimeEpisodeMapping

--- MediaBrowser.Plugins.Anime/AnimeLists/AnimeEpisodeMapping.cs
+++ MediaBrowser.Plugins.Anime/AnimeLists/AnimeEpisodeMapping.cs
@@ -1,8 +1,19 @@
 namespace MediaBrowser.Plugins.Anime.AnimeLists
 {
+    /// <summary>
+    ///     A mapping between two specific episode numbers.
+    /// </summary>
     public class AnimeEpisodeMapping
     {
+        /// <summary>
+        ///     The AniDB episode number
+        /// </summary>
         public int AniDb { get; set; }
+
+        /// <summary>
+        ///     The TvDb episode number, zero indicates that there is no equivalent TvDb episode and that the AniDb episode
+        ///     conflicts with the TvDb episode.
+        /// </summary>
         public int TvDb { get; set; }
     }
 }
