e9cc7b23acd1b1a53856260dbf4fe800b1048d9c
Joshua Rose <joshua.rose@outlook.com>
2017-09-03 21:58:42 +0100

    Remove unused configuration options

--- MediaBrowser.Plugins.AniMetadata/Configuration/PluginConfiguration.cs
+++ MediaBrowser.Plugins.AniMetadata/Configuration/PluginConfiguration.cs
@@ -8,23 +8,15 @@ namespace MediaBrowser.Plugins.AniMetadata.Configuration
         public PluginConfiguration()
         {
             TitlePreference = TitleType.Localized;
-            AllowAutomaticMetadataUpdates = true;
-            TidyGenreList = true;
             MaxGenres = 5;
             MoveExcessGenresToTags = true;
             AddAnimeGenre = true;
-            UseAnidbOrderingWithSeasons = false;
-            UseAnidbDescriptions = false;
         }
 
         public TitleType TitlePreference { get; set; }
-        public bool AllowAutomaticMetadataUpdates { get; set; }
-        public bool TidyGenreList { get; set; }
         public int MaxGenres { get; set; }
         public bool MoveExcessGenresToTags { get; set; }
         public bool AddAnimeGenre { get; set; }
-        public bool UseAnidbOrderingWithSeasons { get; set; }
-        public bool UseAnidbDescriptions { get; set; }
 
         public string TvDbApiKey { get; set; }
     }
