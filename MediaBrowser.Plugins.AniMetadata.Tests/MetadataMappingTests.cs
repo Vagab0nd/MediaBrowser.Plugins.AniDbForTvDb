85b177e084e1c47c8d3ef8e7731c44fd67699d73
Joshua Rose <joshua.rose@outlook.com>
2017-09-13 22:27:01 +0100

    Be able to map metadata properties in one step
    MetadataMapping allows multiple property mappings to be set up ahead of
    time and used to apply both AniDb and TvDb data to a metadata object in
    a single step.

---
+++ MediaBrowser.Plugins.AniMetadata.Tests/MetadataMappingTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Plugins.AniMetadata.MetadataMapping;
+using NUnit.Framework;
+
+namespace MediaBrowser.Plugins.AniMetadata.Tests
+{
+    [TestFixture]
+    public class MetadataMappingTests
+    {
+        private class AniDbSource
+        {
+            public string AniDbValue => "AniDb";
+        }
+
+        private class TvDbSource
+        {
+            public string TvDbValue => "TvDb";
+        }
+
+        private class Metadata : BaseItem
+        {
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Local
+            public string TargetValueA { get; set; } = "TargetValueA";
+
+            public string TargetValueB { get; set; } = "TargetValueB";
+        }
+
+        [Test]
+        public void Map_CopiesSourceDataToTarget()
+        {
+            var aniDbMapping =
+                new PropertyMapping<AniDbSource, Metadata, string, string>(s => s.AniDbValue, t => t.TargetValueA);
+            var tvDbMapping =
+                new PropertyMapping<TvDbSource, Metadata, string, string>(s => s.TvDbValue, t => t.TargetValueB);
+
+            var aniDbSource = new AniDbSource();
+            var tvDbSource = new TvDbSource();
+            var metadata = new Metadata();
+
+            var metadataMapping = MetadataMapping<Metadata>.Create(new[] { aniDbMapping }, new[] { tvDbMapping });
+
+            metadataMapping.Map(aniDbSource, tvDbSource, metadata);
+
+            metadata.TargetValueA.Should().Be("AniDb");
+            metadata.TargetValueB.Should().Be("TvDb");
+        }
+    }
+}
---
+++ MediaBrowser.Plugins.AniMetadata/MetadataMapping/MetadataMapping.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using MediaBrowser.Controller.Entities;
+
+namespace MediaBrowser.Plugins.AniMetadata.MetadataMapping
+{
+    internal static class MetadataMapping<TMetadata> where TMetadata : BaseItem
+    {
+        public static MetadataMapping<TAniDbSource, TTvDbSource, TMetadata> Create<TAniDbSource, TTvDbSource>(
+            IEnumerable<IPropertyMapping<TAniDbSource, TMetadata>> aniDbPropertyMappings,
+            IEnumerable<IPropertyMapping<TTvDbSource, TMetadata>> tvDbPropertyMappings)
+        {
+            return new MetadataMapping<TAniDbSource, TTvDbSource, TMetadata>(aniDbPropertyMappings,
+                tvDbPropertyMappings);
+        }
+    }
+
+    /// <summary>
+    ///     Maps data from AniDb and TvDb sources to a target metadata object according to the provided property mappings
+    /// </summary>
+    internal class MetadataMapping<TAniDbSource, TTvDbSource, TMetadata> where TMetadata : BaseItem
+    {
+        private readonly IEnumerable<IPropertyMapping<TAniDbSource, TMetadata>> _aniDbPropertyMappings;
+        private readonly IEnumerable<IPropertyMapping<TTvDbSource, TMetadata>> _tvDbPropertyMappings;
+
+        public MetadataMapping(IEnumerable<IPropertyMapping<TAniDbSource, TMetadata>> aniDbPropertyMappings,
+            IEnumerable<IPropertyMapping<TTvDbSource, TMetadata>> tvDbPropertyMappings)
+        {
+            _aniDbPropertyMappings = aniDbPropertyMappings;
+            _tvDbPropertyMappings = tvDbPropertyMappings;
+        }
+
+        public void Map(TAniDbSource aniDbSource, TTvDbSource tvDbSource, TMetadata targetMetadata)
+        {
+            _aniDbPropertyMappings.Iter(m => m.Map(aniDbSource, targetMetadata));
+            _tvDbPropertyMappings.Iter(m => m.Map(tvDbSource, targetMetadata));
+        }
+    }
+}
