6fc03ef57dcd34b20b4c493eeb54f5f778ee6bbc
Joshua Rose <joshua.rose@outlook.com>
2017-10-01 08:48:05 +0100

    Show season and episode mappings on config page

--- MediaBrowser.Plugins.AniMetadata.Tests/PluginConfigurationTests.cs
+++ MediaBrowser.Plugins.AniMetadata.Tests/PluginConfigurationTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 using MediaBrowser.Plugins.AniMetadata.Configuration;
 using MediaBrowser.Plugins.AniMetadata.Files;
 using NUnit.Framework;
@@ -377,7 +378,425 @@ namespace MediaBrowser.Plugins.AniMetadata.Tests
       </Mappings>
     </PropertyMappingKeyCollection>
   </SeriesMappings>
+  <SeasonMappings>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Name</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Name</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>TvDB</SourceName>
+          <TargetPropertyName>Name</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Name</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>PremiereDate</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>PremiereDate</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>PremiereDate</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>EndDate</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>EndDate</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>EndDate</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>CommunityRating</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>CommunityRating</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>CommunityRating</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Overview</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Overview</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Overview</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Studios</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Studios</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Studios</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Genres</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Genres</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Genres</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Tags</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Tags</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Tags</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+  </SeasonMappings>
+  <EpisodeMappings>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Name</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Name</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>TvDB</SourceName>
+          <TargetPropertyName>Name</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Name</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>PremiereDate</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>PremiereDate</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>TvDB</SourceName>
+          <TargetPropertyName>PremiereDate</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>PremiereDate</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>RunTimeTicks</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>RunTimeTicks</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>RunTimeTicks</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>CommunityRating</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>CommunityRating</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>TvDB</SourceName>
+          <TargetPropertyName>CommunityRating</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>CommunityRating</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+    <PropertyMappingKeyCollection>
+      <TargetPropertyName>Overview</TargetPropertyName>
+      <Mappings>
+        <PropertyMappingKey>
+          <SourceName>AniDB</SourceName>
+          <TargetPropertyName>Overview</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>TvDB</SourceName>
+          <TargetPropertyName>Overview</TargetPropertyName>
+        </PropertyMappingKey>
+        <PropertyMappingKey>
+          <SourceName>None</SourceName>
+          <TargetPropertyName>Overview</TargetPropertyName>
+        </PropertyMappingKey>
+      </Mappings>
+    </PropertyMappingKeyCollection>
+  </EpisodeMappings>
 </PluginConfiguration>");
+        }
+
+        [Test]
+        public void SeriesMappings_ExtraMappingInConfigured_RemovesExtraMapping()
+        {
+            var configured = new PluginConfiguration().SeriesMappings;
+            configured[0].Mappings = configured[0]
+                .Mappings.Concat(new[] { new PropertyMappingKey("ExtraSource", "ExtraTarget") })
+                .ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeriesMappings = configured;
+
+            pluginConfiguration.SeriesMappings.Should()
+                .NotContain(mc =>
+                    mc.Mappings.Any(m => m.SourceName == "ExtraSource" || m.TargetPropertyName == "ExtraTarget"));
+        }
+
+        [Test]
+        public void SeriesMappings_ExtraPropertyInConfigured_RemovesExtraProperty()
+        {
+            var configured = new PluginConfiguration().SeriesMappings.Concat(new[]
+                {
+                    new PropertyMappingKeyCollection("ExtraProperty",
+                        new[] { new PropertyMappingKey("ExtraSource", "ExtraTarget") })
+                })
+                .ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeriesMappings = configured;
+
+            pluginConfiguration.SeriesMappings.Should().NotContain(m => m.TargetPropertyName == "ExtraProperty");
+        }
+
+        [Test]
+        public void SeriesMappings_MatchingMappinngInConfigured_UpdatesMappingOrderToConfigured()
+        {
+            var configured = new PluginConfiguration().SeriesMappings;
+            configured[0].Mappings = configured[0].Mappings.Reverse().ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeriesMappings = configured;
+
+            pluginConfiguration.SeriesMappings[0].Mappings.ShouldBeEquivalentTo(configured[0].Mappings);
+        }
+
+        [Test]
+        public void SeriesMappings_MissingMappingInConfigured_AddsMissingMappingAtEnd()
+        {
+            var configured = new PluginConfiguration().SeriesMappings;
+            var missing = configured[0].Mappings[0];
+            configured[0].Mappings = configured[0].Mappings.Skip(1).ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeriesMappings = configured;
+
+            pluginConfiguration.SeriesMappings[0].Mappings.Last().ShouldBeEquivalentTo(missing);
+        }
+
+        [Test]
+        public void SeriesMappings_MissingPropertyInConfigured_AddsMissingPropertyAtEnd()
+        {
+            var configured = new PluginConfiguration().SeriesMappings;
+            var missing = configured[0];
+            configured = configured.Skip(1).ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeriesMappings = configured;
+
+            pluginConfiguration.SeriesMappings.Last().ShouldBeEquivalentTo(missing);
+        }
+
+        [Test]
+        public void SeasonMappings_ExtraMappingInConfigured_RemovesExtraMapping()
+        {
+            var configured = new PluginConfiguration().SeasonMappings;
+            configured[0].Mappings = configured[0]
+                .Mappings.Concat(new[] { new PropertyMappingKey("ExtraSource", "ExtraTarget") })
+                .ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeasonMappings = configured;
+
+            pluginConfiguration.SeasonMappings.Should()
+                .NotContain(mc =>
+                    mc.Mappings.Any(m => m.SourceName == "ExtraSource" || m.TargetPropertyName == "ExtraTarget"));
+        }
+
+        [Test]
+        public void SeasonMappings_ExtraPropertyInConfigured_RemovesExtraProperty()
+        {
+            var configured = new PluginConfiguration().SeasonMappings.Concat(new[]
+                {
+                    new PropertyMappingKeyCollection("ExtraProperty",
+                        new[] { new PropertyMappingKey("ExtraSource", "ExtraTarget") })
+                })
+                .ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeasonMappings = configured;
+
+            pluginConfiguration.SeasonMappings.Should().NotContain(m => m.TargetPropertyName == "ExtraProperty");
+        }
+
+        [Test]
+        public void SeasonMappings_MatchingMappinngInConfigured_UpdatesMappingOrderToConfigured()
+        {
+            var configured = new PluginConfiguration().SeasonMappings;
+            configured[0].Mappings = configured[0].Mappings.Reverse().ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeasonMappings = configured;
+
+            pluginConfiguration.SeasonMappings[0].Mappings.ShouldBeEquivalentTo(configured[0].Mappings);
+        }
+
+        [Test]
+        public void SeasonMappings_MissingMappingInConfigured_AddsMissingMappingAtEnd()
+        {
+            var configured = new PluginConfiguration().SeasonMappings;
+            var missing = configured[0].Mappings[0];
+            configured[0].Mappings = configured[0].Mappings.Skip(1).ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeasonMappings = configured;
+
+            pluginConfiguration.SeasonMappings[0].Mappings.Last().ShouldBeEquivalentTo(missing);
+        }
+
+        [Test]
+        public void SeasonMappings_MissingPropertyInConfigured_AddsMissingPropertyAtEnd()
+        {
+            var configured = new PluginConfiguration().SeasonMappings;
+            var missing = configured[0];
+            configured = configured.Skip(1).ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.SeasonMappings = configured;
+
+            pluginConfiguration.SeasonMappings.Last().ShouldBeEquivalentTo(missing);
+        }
+
+        [Test]
+        public void EpisodeMappings_ExtraMappingInConfigured_RemovesExtraMapping()
+        {
+            var configured = new PluginConfiguration().EpisodeMappings;
+            configured[0].Mappings = configured[0]
+                .Mappings.Concat(new[] { new PropertyMappingKey("ExtraSource", "ExtraTarget") })
+                .ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.EpisodeMappings = configured;
+
+            pluginConfiguration.EpisodeMappings.Should()
+                .NotContain(mc =>
+                    mc.Mappings.Any(m => m.SourceName == "ExtraSource" || m.TargetPropertyName == "ExtraTarget"));
+        }
+
+        [Test]
+        public void EpisodeMappings_ExtraPropertyInConfigured_RemovesExtraProperty()
+        {
+            var configured = new PluginConfiguration().EpisodeMappings.Concat(new[]
+                {
+                    new PropertyMappingKeyCollection("ExtraProperty",
+                        new[] { new PropertyMappingKey("ExtraSource", "ExtraTarget") })
+                })
+                .ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.EpisodeMappings = configured;
+
+            pluginConfiguration.EpisodeMappings.Should().NotContain(m => m.TargetPropertyName == "ExtraProperty");
+        }
+
+        [Test]
+        public void EpisodeMappings_MatchingMappinngInConfigured_UpdatesMappingOrderToConfigured()
+        {
+            var configured = new PluginConfiguration().EpisodeMappings;
+            configured[0].Mappings = configured[0].Mappings.Reverse().ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.EpisodeMappings = configured;
+
+            pluginConfiguration.EpisodeMappings[0].Mappings.ShouldBeEquivalentTo(configured[0].Mappings);
+        }
+
+        [Test]
+        public void EpisodeMappings_MissingMappingInConfigured_AddsMissingMappingAtEnd()
+        {
+            var configured = new PluginConfiguration().EpisodeMappings;
+            var missing = configured[0].Mappings[0];
+            configured[0].Mappings = configured[0].Mappings.Skip(1).ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.EpisodeMappings = configured;
+
+            pluginConfiguration.EpisodeMappings[0].Mappings.Last().ShouldBeEquivalentTo(missing);
+        }
+
+        [Test]
+        public void EpisodeMappings_MissingPropertyInConfigured_AddsMissingPropertyAtEnd()
+        {
+            var configured = new PluginConfiguration().EpisodeMappings;
+            var missing = configured[0];
+            configured = configured.Skip(1).ToArray();
+
+            var pluginConfiguration = new PluginConfiguration();
+
+            pluginConfiguration.EpisodeMappings = configured;
+
+            pluginConfiguration.EpisodeMappings.Last().ShouldBeEquivalentTo(missing);
         }
     }
 }
--- MediaBrowser.Plugins.AniMetadata/Configuration/PluginConfiguration.cs
+++ MediaBrowser.Plugins.AniMetadata/Configuration/PluginConfiguration.cs
@@ -1,12 +1,17 @@
 ﻿using System.Linq;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Plugins.AniMetadata.AniDb;
+using MediaBrowser.Plugins.AniMetadata.PropertyMapping;
 using MediaBrowser.Plugins.AniMetadata.TvDb;
 
 namespace MediaBrowser.Plugins.AniMetadata.Configuration
 {
     public class PluginConfiguration : BasePluginConfiguration
     {
+        private PropertyMappingKeyCollection[] _episodeMappings;
+        private PropertyMappingKeyCollection[] _seasonMappings;
+        private PropertyMappingKeyCollection[] _seriesMappings;
+
         public PluginConfiguration()
         {
             TitlePreference = TitleType.Localized;
@@ -14,7 +19,15 @@ namespace MediaBrowser.Plugins.AniMetadata.Configuration
             MoveExcessGenresToTags = true;
             AddAnimeGenre = true;
 
-            SeriesMappings = GetDefaultSeriesMappings();
+            var mappingConfiguration = new MappingConfiguration(new ISourceMappingConfiguration[]
+            {
+                new AniDbSourceMappingConfiguration(new AniDbParser()),
+                new TvDbSourceMappingConfiguration()
+            });
+
+            _seriesMappings = GetDefaultSeriesMappings(mappingConfiguration);
+            _seasonMappings = GetDefaultSeasonMappings(mappingConfiguration);
+            _episodeMappings = GetDefaultEpisodeMappings(mappingConfiguration);
         }
 
         public TitleType TitlePreference { get; set; }
@@ -24,24 +37,92 @@ namespace MediaBrowser.Plugins.AniMetadata.Configuration
 
         public string TvDbApiKey { get; set; }
 
-        public PropertyMappingKeyCollection[] SeriesMappings { get; set; }
+        public PropertyMappingKeyCollection[] SeriesMappings
+        {
+            get => _seriesMappings;
+            set => _seriesMappings = MergeMappings(_seriesMappings, value);
+        }
 
-        private PropertyMappingKeyCollection[] GetDefaultSeriesMappings()
+        public PropertyMappingKeyCollection[] SeasonMappings
+        {
+            get => _seasonMappings;
+            set => _seasonMappings = MergeMappings(_seasonMappings, value);
+        }
+
+        public PropertyMappingKeyCollection[] EpisodeMappings
+        {
+            get => _episodeMappings;
+            set => _episodeMappings = MergeMappings(_episodeMappings, value);
+        }
+
+        private PropertyMappingKeyCollection[] GetDefaultSeriesMappings(IMappingConfiguration mappingConfiguration)
         {
             var propertyMappings =
-                new MappingConfiguration(new ISourceMappingConfiguration[]
-                {
-                    new AniDbSourceMappingConfiguration(new AniDbParser()),
-                    new TvDbSourceMappingConfiguration()
-                }).GetSeriesMappings(MaxGenres, MoveExcessGenresToTags,
-                    AddAnimeGenre);
+                mappingConfiguration.GetSeriesMappings(MaxGenres, MoveExcessGenresToTags, AddAnimeGenre);
 
+            return ToCollection(propertyMappings);
+        }
+
+        private PropertyMappingKeyCollection[] GetDefaultSeasonMappings(IMappingConfiguration mappingConfiguration)
+        {
+            var propertyMappings = mappingConfiguration.GetSeasonMappings(MaxGenres, AddAnimeGenre);
+
+            return ToCollection(propertyMappings);
+        }
+
+        private PropertyMappingKeyCollection[] GetDefaultEpisodeMappings(IMappingConfiguration mappingConfiguration)
+        {
+            var propertyMappings = mappingConfiguration.GetEpisodeMappings();
+
+            return ToCollection(propertyMappings);
+        }
+
+        private PropertyMappingKeyCollection[] ToCollection(IPropertyMappingCollection propertyMappings)
+        {
             return propertyMappings
                 .Select(m => new PropertyMappingKey(m.SourceName, m.TargetPropertyName))
                 .GroupBy(m => m.TargetPropertyName)
                 .Select(g =>
                     new PropertyMappingKeyCollection(g.Key, g.Concat(new[] { new PropertyMappingKey("None", g.Key) })))
                 .ToArray();
+        }
+
+        private PropertyMappingKeyCollection[] MergeMappings(PropertyMappingKeyCollection[] defaults,
+            PropertyMappingKeyCollection[] configured)
+        {
+            if (configured == null)
+            {
+                return defaults;
+            }
+
+            return configured.Where(m => defaults.Any(dm => AreEquivalent(dm, m)))
+                .Concat(defaults.Where(dm =>
+                    !configured.Any(m => AreEquivalent(dm, m))))
+                .Select(m => MergeMappings(defaults.Single(d => AreEquivalent(d, m)), m))
+                .ToArray();
+        }
+
+        private PropertyMappingKeyCollection MergeMappings(PropertyMappingKeyCollection defaults,
+            PropertyMappingKeyCollection configured)
+        {
+            var mergedMappings = configured.Mappings.Where(m => defaults.Mappings.Any(dm => AreEquivalent(dm, m)))
+                .Concat(defaults.Mappings.Where(dm =>
+                    !configured.Mappings.Any(m => AreEquivalent(dm, m))));
+
+            configured.Mappings = mergedMappings.ToArray();
+
+            return configured;
+        }
+
+        private bool AreEquivalent(PropertyMappingKey keyA, PropertyMappingKey keyB)
+        {
+            return keyA.SourceName == keyB.SourceName && keyA.TargetPropertyName == keyB.TargetPropertyName;
+        }
+
+        private bool AreEquivalent(PropertyMappingKeyCollection keyCollectionA,
+            PropertyMappingKeyCollection keyCollectionB)
+        {
+            return keyCollectionA.TargetPropertyName == keyCollectionB.TargetPropertyName;
         }
     }
 }
