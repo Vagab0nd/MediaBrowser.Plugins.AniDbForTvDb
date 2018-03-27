61cfc950fd306830098d6e4ba053217a77e34fcd
Joshua Rose <joshua.rose@outlook.com>
2018-03-27 20:45:13 +0100

    Ensure external Ids use the correct source names

--- MediaBrowser.Plugins.AniMetadata/EmbyExternalIds/AniDbSeriesExternalId.cs
+++ MediaBrowser.Plugins.AniMetadata/EmbyExternalIds/AniDbSeriesExternalId.cs
@@ -1,8 +1,9 @@
 ﻿using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
+using MediaBrowser.Plugins.AniMetadata.Process.Sources;
 
-namespace MediaBrowser.Plugins.AniMetadata.Providers.AniDb
+namespace MediaBrowser.Plugins.AniMetadata.EmbyExternalIds
 {
     public class AniDbSeriesExternalId : IExternalId
     {
@@ -11,9 +12,9 @@ namespace MediaBrowser.Plugins.AniMetadata.Providers.AniDb
             return item is Series;
         }
 
-        public string Name => ProviderNames.AniDb;
+        public string Name => SourceNames.AniDb;
 
-        public string Key => ProviderNames.AniDb;
+        public string Key => SourceNames.AniDb;
 
         public string UrlFormatString => "http://anidb.net/perl-bin/animedb.pl?show=anime&aid={0}";
     }
